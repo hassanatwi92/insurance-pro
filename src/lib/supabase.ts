00705b94c2dfbecb624efe0bca40710ffbca7bd1
hassanatwi92 <has.92@hotmail.com>
2026-04-12 01:07:26 +0300

    Rename supabase.js to supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://pgdllmufapzxpzeubgei.supabase.co";
+const supabaseAnonKey = "sb_publishable_t28plV_dSwu-GWmem62Bbw_aMdfV67s";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
